637e78930d391fb22433f623acf4ff0d15111755
abhi <s530670@nwmissouri.edu>
2018-06-26 15:40:42 -0500

    Updated the script and readme file

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,18 +1,20 @@
 const Twit = require('twit')
 const fs = require('fs');
+
+const hashTag = "#girlPower"
+
 var T = new Twit({
-    consumer_key:"HTNc6rm4tX63VYQKWQCV40TFm",
-    consumer_secret:"bAWS75kNkDMKrWvGvzoGVYrRdjrtrZk7BaPwTBcBzdpfdHr0Il",
-    access_token:"969603656080330753-ucYDl5fW5aJmzT2AJIuR6wtKaa8Jf7A",
-    access_token_secret: "HrI9XHNBqtQ0xxouDPqeY6OOOzLav1moLk1BgP7l29an9",
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
-  })
+  consumer_key: "HTNc6rm4tX63VYQKWQCV40TFm",
+  consumer_secret: "bAWS75kNkDMKrWvGvzoGVYrRdjrtrZk7BaPwTBcBzdpfdHr0Il",
+  access_token: "969603656080330753-ucYDl5fW5aJmzT2AJIuR6wtKaa8Jf7A",
+  access_token_secret: "HrI9XHNBqtQ0xxouDPqeY6OOOzLav1moLk1BgP7l29an9",
+  timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL: true,     // optional - requires SSL certificates to be valid.
+})
 
-  T.get('search/tweets', { q: '#girlpower', count: 1000 }, function(err, data, response) {
-    console.log(data.statuses)
-    var json = JSON.stringify(data.statuses);
-    fs.writeFile('./output.json', json, 'utf8',function(){
-
-    });
-  })    
+T.get('search/tweets', { q: hashTag, count: 1000 }, function (err, data, response) {
+  var twitsData = JSON.stringify(data.statuses);
+  fs.writeFile('./output.json', twitsData, 'utf8', function () {
+    console.log("Output generated successfully")
+  });
+})    
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# StreamTwitterDataNodeJS
+# Retrieve Twitter Data
+We have designed two scripts for pulling the twitter post based on the hashtag.
+
+## NodeJS version
+- Go to NodeJS folder
+- Open the command prompt and run "npm install"
+- Run "npm start"
+- You will get output.json file as a output.
+
+## Python version
